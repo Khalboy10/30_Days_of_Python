6e942a9f881e72dabf921759d3e7deb0a5b47aae
khalboy10 <auwalauwal10@gmail.com>
2023-02-08 06:34:52 +0100

    Day one problems solutions

---
+++ day_1/helloworld.py
@@ -0,0 +1,53 @@
+from math import sqrt
+
+# Exercises: Level 1 and 2
+# Exercise 1
+print('python3 --version')
+
+# Exercise 2
+
+print(3 + 4) # Addition
+print(3 - 4) # Subtrsction
+print(3 * 4) # Multiplication
+print(3 % 4) # Modulus
+print(3 / 4) # Division
+print(3 ** 4) # Exponantial
+print(3 // 4) # floor division
+
+
+# Exercise 3
+
+print('Auwal Auwal Sulaiman')
+print('Mebarewa')
+print('Nigeria')
+print('i am enjoying 30 days of python')
+
+
+# Exercise 4
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Auwal Auwal Sulaiman'))
+print(type('Mebarewa'))
+print(type('Nigeria'))
+
+# Exercises: Level 3
+# Exercise 1
+
+print(3)                            # integer
+print(4.7)                          # floor
+print(4 - 4j)                        # complex
+print('Arewa data Science')          # string
+print(True, False)                  # boolean
+print([1, 2, 3])                    # list
+print((9.8, 3.14, 2.7))              # tuple
+print({9.8, 3.14, 2.7})             # set
+print({'fellow' : 'Auwal'})         # dictionary
+
+# Exercise 2
+a = (2 - 3) ** 2
+b = (10 - 8) ** 2
+print(sqrt(a + b))
