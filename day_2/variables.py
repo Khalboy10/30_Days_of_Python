e6d77b7eaca5f66862086a82643f6b0df37f8d32
khalboy10 <auwalauwal10@gmail.com>
2023-02-08 06:45:36 +0100

    Day two problems solutions

---
+++ day_2/variables.py
@@ -0,0 +1,52 @@
+# Exercises: Level 1
+
+# day_2: 30 Days of python programming
+first_name = 'Auwal'
+last_name = 'Sulaiman'
+full_name = 'Auwal Auwal Sulaiman'
+country = 'Nigeria'
+city = 'Kano'
+age = 23
+year = 2023
+is_married = False
+is_true = True
+is_light_on = True
+first_name, last_name, full_name = 'Auwal', 'Sulaiman', 'Auwal Auwal Sulaiman'
+
+# Exercises: Level 2
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+area_of_circle = 3.14 * (30 ** 2)
+circum_of_circle = 2 * 3.14 * 30
+radius = int(input('Enter Radius: '))
+area = 3.14 * (radius ** 2)
+
+first_name = input('Enter first name: ')
+last_name = input('Enter last name: ')
+country = input('Enter country name: ')
+age = int(input('Enter age: '))
+
+help('keywords')
